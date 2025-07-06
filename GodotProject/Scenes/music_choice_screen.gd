59bbb9d772575bde8a4f8552e2ba93d770909da1
Oz <squidchild2@gmail.com>
2025-07-06 16:26:19 +0200

    Song choice screen

--- GodotProject/Scenes/music_choice_screen.gd
+++ GodotProject/Scenes/music_choice_screen.gd
@@ -4,9 +4,20 @@ const songs_folder_path = "res://Songs/"
 
 @onready var v_box_container : VBoxContainer = $ScrollContainer/MarginContainer/VBoxContainer
 
+func _ready() -> void:
+	find_songs()
+
 func find_songs() -> void:
 	var songs = DirAccess.get_files_at(songs_folder_path)
 	for song in songs:
-		var button = Button.new()
-		button.text = song.replace(".mp3", "").to_upper().replace("_", " ")
-		v_box_container.add_child(button)
+		if song.contains(".import") or song.contains(".cfg"):
+			pass
+		else:
+			var button = Button.new()
+			button.text = song.replace(".mp3", "").to_upper()
+			v_box_container.add_child(button)
+			
+			button.pressed.connect(func():
+				SongChoice.chosen_song = button.text
+				get_tree().change_scene_to_file("res://Scenes/RhythmGame.tscn")
+			)
--- GodotProject/Scenes/rhythm_game.gd
+++ GodotProject/Scenes/rhythm_game.gd
@@ -3,18 +3,35 @@ extends Node2D
 @onready var target: Node2D = $Target
 @onready var conductor: AudioStreamPlayer = $Conductor
 
-@export var beatsBeforeStart: int
+@export var songName: String
+
+var beatsBeforeStart: int
+var songBpm: int
+var notesOnBeat: Array[int]
 
 var note = preload("res://Scenes/RhythmGameControls/Note.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	songName = SongChoice.chosen_song
+	load_song_file()
+	conductor.stream = load("res://Songs/" + songName + ".mp3")
 	conductor.play_with_beat_offset(beatsBeforeStart)
 
 func _on_conductor_beat_signal(position: int) -> void:
-	var note_instance = note.instantiate()
-	note_instance.global_position = target.global_position
-	note_instance.global_position.x += 1920
-	note_instance.speed = 1920 / (conductor.secondsPerBeat * beatsBeforeStart)
+	if notesOnBeat.has(position):
+		var note_instance = note.instantiate()
+		note_instance.global_position = target.global_position
+		note_instance.global_position.x += 1920
+		note_instance.speed = 1920 / (conductor.secondsPerBeat * beatsBeforeStart)
 	
-	add_child(note_instance)
+		add_child(note_instance)
+
+func load_song_file():
+	var file = FileAccess.open("res://Songs/" + songName + ".cfg", FileAccess.READ)
+	songBpm = int(file.get_line())
+	beatsBeforeStart = int(file.get_line())
+	
+	while file.eof_reached() == false:
+		notesOnBeat.append(int(file.get_line()))
+	notesOnBeat.remove_at(0) # Godot creates a first element that we do not want
